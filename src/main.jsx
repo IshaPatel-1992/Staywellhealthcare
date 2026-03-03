d2fbba508323ba41a2bf571c0d16b06da6cb4a5f
IshaPatel-1992 <patelisha1992@gmail.com>
2026-03-02 21:52:15 -0700

    Changes to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./index.css"
-import "@fontsource/figtree/400.css"
-import "@fontsource/figtree/500.css"
-import "@fontsource/figtree/600.css"
-import { HelmetProvider } from "react-helmet-async";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "@fontsource/figtree/400.css";
+import "@fontsource/figtree/500.css";
+import "@fontsource/figtree/600.css";
 
+import App from "./App.jsx";
 
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </HelmetProvider>,
-)
+  </StrictMode>
+);
